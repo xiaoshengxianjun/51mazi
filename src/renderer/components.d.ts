ffb3a6cec49a7245b433a3358111365800feb14b
xsxj <xsxjlzy@163.com>
2025-11-29 11:09:30 +0800

    更新

--- src/renderer/components.d.ts
+++ src/renderer/components.d.ts
@@ -21,7 +21,6 @@ declare module 'vue' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCol: typeof import('element-plus/es')['ElCol']
     ElDialog: typeof import('element-plus/es')['ElDialog']
-    ElDivider: typeof import('element-plus/es')['ElDivider']
     ElDropdown: typeof import('element-plus/es')['ElDropdown']
     ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
     ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
